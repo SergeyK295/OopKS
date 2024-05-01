4c38dce3dfffb2a163afbce6b95a6bc937695675
SergeyK295 <krivet-ser@mail.ru>
2024-05-01 19:17:13 +0700

    Исправлены замечания от 14 апреля

--- ArrayListHome/src/ru/academits/kolesnikov/array_list_home_main/Main.java
+++ ArrayListHome/src/ru/academits/kolesnikov/array_list_home_main/Main.java
@@ -5,56 +5,59 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void readLinesFromFile(ArrayList<String> strings, String file) {
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
-            String string;
-            while ((string = bufferedReader.readLine()) != null) {
-                strings.add(string);
+    public static ArrayList<String> getStringListFromFile(String fileName) throws IOException {
+        ArrayList<String> lines = new ArrayList<>();
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+        String line;
+
+        while ((line = bufferedReader.readLine()) != null) {
+            lines.add(line);
+        }
+
+        return lines;
+    }
+
+    public static void deleteEvenNumbers(ArrayList<Integer> numbers) {
+        for (int i = numbers.size() - 1; i >= 0; i--) {
+            if (numbers.get(i) % 2 == 0) {
+                numbers.remove(i);
             }
-        } catch (FileNotFoundException e) {
-            System.out.println("Ошибка! Файл не найден!");
-        } catch (IOException e) {
-            System.out.println("Файл не прочитан. Ошибка при вводе/выводе данных из файла!");
         }
     }
 
-    public static void deleteEvenNumbers(ArrayList<Integer> list) {
-        for (int i = list.size() - 1; i > -1; i--) {
-            if (list.get(i) % 2 == 0) {
-                list.remove(i);
-            }
-        }
-    }
+    public static <T> ArrayList<T> getNonRepetitiveList(ArrayList<T> arrayList) {
+        ArrayList<T> nonRepetitiveList = new ArrayList<>(arrayList.size());
 
-    public static ArrayList<Integer> deleteDuplicate(ArrayList<Integer> list) {
-        ArrayList<Integer> result = new ArrayList<>();
-
-        ArrayList<Integer> tamp = (ArrayList<Integer>) list.clone();
-
-        for (int i = 0; !tamp.isEmpty(); i++) {
-            result.add(tamp.get(0));
-
-            while (tamp.remove(result.get(i))) {
-            }
+        for (T t : arrayList) {
+            if (!nonRepetitiveList.contains(t))
+                nonRepetitiveList.add(t);
         }
 
-        return result;
+        return nonRepetitiveList;
     }
 
     public static void main(String[] args) {
-        ArrayList<String> strings = new ArrayList<>();
-        String file = "line.txt";
-        readLinesFromFile(strings, file);
+        String fileName = "line.txt";
+        ArrayList<String> strings = null;
+
+        try {
+            strings = getStringListFromFile(fileName);
+        } catch (FileNotFoundException e) {
+            System.out.println("Ошибка! Файл не найден!");
+        } catch (IOException e) {
+            System.out.println("Файл не прочитан." + e.getMessage());
+        }
+
         System.out.println("Строки из файла: " + strings);
 
-        Integer[] numberArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 1, 12, 13, 4, 6, 7, 3, 17, 9, 10};
+        Integer[] numbersArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 1, 12, 13, 4, 6, 7, 3, 17, 9, 10};
 
-        ArrayList<Integer> numberList1 = new ArrayList<>(List.of(numberArray));
-        System.out.println("Изначальный список чисел: " + numberList1);
-        deleteEvenNumbers(numberList1);
-        System.out.println("Список без четных чисел: " + numberList1);
+        ArrayList<Integer> numbersList1 = new ArrayList<>(List.of(numbersArray));
+        System.out.println("Изначальный список чисел: " + numbersList1);
+        deleteEvenNumbers(numbersList1);
+        System.out.println("Список без четных чисел: " + numbersList1);
 
-        ArrayList<Integer> numberList2 = deleteDuplicate(numberList1);
-        System.out.println("Список чисел без повторения: " + numberList2);
+        ArrayList<Integer> numbersList2 = getNonRepetitiveList(numbersList1);
+        System.out.println("Список чисел без повторения: " + numbersList2);
     }
 }
