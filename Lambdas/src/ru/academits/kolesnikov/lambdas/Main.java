ccd31add904710442dcecf775a68e13a5aa2db24
SergeyK295 <krivet-ser@mail.ru>
2024-05-30 17:48:00 +0700

    Исправлены замечания по письму от 16.05.2024

---
+++ Lambdas/src/ru/academits/kolesnikov/lambdas/Main.java
@@ -0,0 +1,69 @@
+package ru.academits.kolesnikov.lambdas;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static List<String> getUniqueNames(List<Person> persons) {
+        return persons.stream()
+                .map(Person::name)
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
+    public static List<Person> getPeopleYounger(List<Person> persons, int age) {
+        return persons.stream()
+                .filter(p -> p.age() < age)
+                .toList();
+    }
+
+    public static Map<String, Double> getAverageAgesByNames(List<Person> persons) {
+        Map<String, List<Person>> personsByNames = persons.stream().collect(Collectors.groupingBy(Person::name));
+
+        return personsByNames.keySet().stream()
+                .collect(Collectors.toMap(name -> name, name -> personsByNames.get(name).stream().mapToInt(Person::age).average().orElseThrow()));
+    }
+
+    public static void main(String[] args) {
+        List<Person> persons = Arrays.asList(
+                new Person("Костя", 18),
+                new Person("Ира", 16),
+                new Person("Паша", 20),
+                new Person("Сергей", 22),
+                new Person("Дима", 15),
+                new Person("Алиса", 30),
+                new Person("Ира", 29),
+                new Person("Сергей", 31),
+                new Person("Вася", 31),
+                new Person("Максим", 20),
+                new Person("Вася", 70));
+
+        List<String> uniqueNames = getUniqueNames(persons);
+        System.out.println(uniqueNames.stream().collect(Collectors.joining(", ", "Имена: ", ".")));
+
+        List<Person> peopleAgeUpTo18 = getPeopleYounger(persons, 18);
+        OptionalDouble averagePeopleAgeUpTo18 = peopleAgeUpTo18.stream()
+                .mapToDouble(Person::age)
+                .average();
+
+        if (averagePeopleAgeUpTo18.isPresent()) {
+            System.out.println("Средний возраст людей младше 18 лет: " + averagePeopleAgeUpTo18.getAsDouble());
+        } else {
+            System.out.println("Нет людей младше 18");
+        }
+
+        Map<String, Double> averageAgesByNames = getAverageAgesByNames(persons);
+        System.out.println("Средний возраст по именам: " + averageAgesByNames);
+
+        String name = "Ира";
+        System.out.println("Средний возраст для имени \"" + name + "\": " + averageAgesByNames.get(name));
+
+        List<String> personsBetween20And45Names = persons.stream()
+                .filter(p -> p.age() >= 20 && p.age() <= 45)
+                .sorted((p1, p2) -> p2.age() - p1.age())
+                .map(Person::name)
+                .toList();
+
+        System.out.println("Имена людей возраст которых от 20 до 45 в порядке убывания возраста: " + personsBetween20And45Names);
+    }
+}
--- Lambdas/src/ru/academits/kolesnikov/root_numbers_flux/RootNumbersFlux.java
+++ Lambdas/src/ru/academits/kolesnikov/root_numbers_flux/RootNumbersFlux.java
@@ -1,19 +1,18 @@
-package ru.academits.kolesnikov.objective;
+package ru.academits.kolesnikov.root_numbers_flux;
 
 import java.util.Scanner;
 import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
 
-public class Objective {
+public class RootNumbersFlux {
     public static void main(String[] args) {
-        DoubleStream squares = IntStream.iterate(0, x -> x + 1)
-                .mapToDouble(Math::sqrt);
+        DoubleStream squareRoots = IntStream.iterate(0, x -> x + 1).mapToDouble(Math::sqrt);
 
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Укажите количество элементов для вычисления квадратных корней:");
         int itemsCount = scanner.nextInt();
 
-        squares.limit(itemsCount)
-                .forEach(System.out::println);
+        squareRoots.limit(itemsCount).forEach(System.out::println);
     }
 }
