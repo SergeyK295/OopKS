e3c9b392292f56aec08bf05064fcbd72e6b8579f
SergeyK295 <krivet-ser@mail.ru>
2024-09-04 20:57:02 +0700

    Исправлены замечания

---
+++ List/src/ru/academits/kolesnikov/list/SinglyLinkedList.java
@@ -0,0 +1,180 @@
+package ru.academits.kolesnikov.list;
+
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class SinglyLinkedList<E> {
+    private ListItem<E> head;
+    private int size;
+
+    public SinglyLinkedList() {
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public E getFirst() {
+        checkForOccupancyList();
+
+        return head.getData();
+    }
+
+    public E get(int index) {
+        checkIndex(index);
+        return getItem(index).getData();
+    }
+
+    public E set(int index, E data) {
+        checkIndex(index);
+
+        ListItem<E> item = getItem(index);
+        E oldData = item.getData();
+        item.setData(data);
+
+        return oldData;
+    }
+
+    public E deleteByIndex(int index) {
+        checkIndex(index);
+
+        if (index == 0) {
+            return deleteFirst();
+        }
+
+        ListItem<E> previousItem = getItem(index - 1);
+        ListItem<E> deletedItem = previousItem.getNext();
+        previousItem.setNext(deletedItem.getNext());
+        size--;
+        return deletedItem.getData();
+    }
+
+    public void addFirst(E data) {
+        head = new ListItem<>(data, head);
+        size++;
+    }
+
+    public void add(int index, E data) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Нет индекса со значением " + index + ". Допустимое значение от 0 до " + size + " включительно.");
+        }
+
+        ListItem<E> previousItem = getItem(index - 1);
+        if (index == 0) {
+            previousItem.setNext(new ListItem<>(previousItem.getData(), previousItem.getNext()));
+            head.setData(data);
+        } else {
+            previousItem.setNext(new ListItem<>(data, previousItem.getNext()));
+        }
+
+        size++;
+    }
+
+    public boolean deleteByData(E data) {
+        if (size == 0) {
+            System.out.println("Список пуст.");
+            return false;
+        }
+
+        if (Objects.equals(data, head.getData())) {
+            deleteFirst();
+            return true;
+        }
+
+        for (ListItem<E> previousItem = head, item = previousItem.getNext(); item != null; previousItem = item, item = item.getNext()) {
+            if (Objects.equals(data, item.getData())) {
+                previousItem.setNext(item.getNext());
+                size--;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public E deleteFirst() {
+        checkForOccupancyList();
+
+        E deletedItem = head.getData();
+        head = head.getNext();
+        size--;
+
+        return deletedItem;
+    }
+
+    public void reverse() {
+        ListItem<E> item = head;
+        ListItem<E> previousItem = null;
+
+        while (item != null) {
+            ListItem<E> nextItem = item.getNext();
+            item.setNext(previousItem);
+            previousItem = item;
+            item = nextItem;
+        }
+
+        head = previousItem;
+    }
+
+    public SinglyLinkedList<E> copy() {
+        if (size == 0) {
+            return new SinglyLinkedList<>();
+        }
+
+        SinglyLinkedList<E> listCopy = new SinglyLinkedList<>();
+        listCopy.addFirst(head.getData());
+        ListItem<E> copiedItem = listCopy.head;
+
+        for (ListItem<E> item = head.getNext(); item != null; item = item.getNext()) {
+            E data = item.getData();
+            copiedItem.setNext(new ListItem<>(data));
+            copiedItem = copiedItem.getNext();
+        }
+
+        listCopy.size = size;
+        return listCopy;
+    }
+
+    private ListItem<E> getItem(int index) {
+        ListItem<E> item = head;
+
+        for (int i = 0; i < index; i++) {
+            item = item.getNext();
+        }
+
+        return item;
+    }
+
+    @Override
+    public String toString() {
+        if (size == 0) {
+            return "[]";
+        }
+
+        StringBuilder stringBuilder = new StringBuilder("[");
+        ListItem<E> item = head;
+
+        for (int i = 0; i < size; i++) {
+            stringBuilder.append(item.getData()).append(", ");
+            item = item.getNext();
+        }
+
+        int stringLength = stringBuilder.length();
+        stringBuilder.delete(stringLength - 2, stringLength);
+
+        stringBuilder.append("]");
+        return stringBuilder.toString();
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Нет индекса со значением " + index + ". Допустимое значение от 0 до " + size);
+        }
+    }
+
+    private void checkForOccupancyList() {
+        if (size == 0) {
+            throw new NoSuchElementException("Список пуст.");
+        }
+    }
+}
--- List/src/ru/academits/kolesnikov/list_main/Main.java
+++ List/src/ru/academits/kolesnikov/list_main/Main.java
@@ -1,10 +1,10 @@
 package ru.academits.kolesnikov.list_main;
 
-import ru.academits.kolesnikov.list.SinglyLinked;
+import ru.academits.kolesnikov.list.SinglyLinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        SinglyLinked<Integer> list1 = new SinglyLinked<>();
+        SinglyLinkedList<Integer> list1 = new SinglyLinkedList<>();
 
         list1.addFirst(0);
 
@@ -15,12 +15,12 @@ public class Main {
         list1.add(list1.size(), null);
         list1.add(list1.size(), 11);
 
-        SinglyLinked<Integer> list2 = list1.copy();
+        SinglyLinkedList<Integer> list2 = list1.copy();
         System.out.println("Копия списка №1");
         System.out.println(list2);
 
-        int delete = 4;
-        System.out.println("Удалим элемент со значением: \"4\" " + list1.deleteByData(list1.get(delete)));
+        int deleteItem = 4;
+        System.out.println("Удалим элемент со значением: \"4\" " + list1.deleteByData(list1.get(deleteItem)));
         System.out.println(list1);
     }
 }
