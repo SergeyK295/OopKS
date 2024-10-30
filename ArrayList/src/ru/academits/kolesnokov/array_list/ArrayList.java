7b2c50ee0ef1522af20916179bff660e9504488c
SergeyK295 <krivet-ser@mail.ru>
2024-10-30 19:21:38 +0700

    исправлена опечатка

--- ArrayList/src/ru/academits/kolesnokov/array_list/ArrayList.java
+++ ArrayList/src/ru/academits/kolesnokov/array_list/ArrayList.java
@@ -355,7 +355,7 @@ public class ArrayList<E> implements List<E> {
 
     @Override
     public int hashCode() {
-        final int prime = 18;
+        final int prime = 17;
         int hash = 1;
         hash = prime * hash + size;
 
